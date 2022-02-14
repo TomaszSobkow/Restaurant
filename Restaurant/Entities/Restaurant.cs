444d383dca7bc7fb6d1cb325869d934f570ab5f5
Tomasz <sobeczki@gmail.com>
2022-02-14 17:51:05 +0000

    Added class Restaurant

---
+++ Restaurant/Entities/Restaurant.cs
@@ -0,0 +1,14 @@
+﻿namespace Restaurant.Entities
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public string category { get; set; }
+        public bool hasDelivery { get; set; }
+        public string email { get; set; }
+        public string phoneNumber { get; set; }
+
+    }
+}
